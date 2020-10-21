4074342191b6effcc75e0f6b6b25e86a9dbb57cb
sirmcclay <mcclay.design@gmail.com>
2020-10-21 12:28:58 -0300

    Using timing, spring and decay

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
-import React, { useEffect, useRef } from 'react';
-import {
-  View,
-  Animated,
-  StyleSheet,
-} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {View, Animated, StyleSheet} from 'react-native';
 
 const App: React.FC = () => {
   const ballY = useRef(new Animated.Value(0)).current;
-  
+
   useEffect(() => {
-    Animated.timing(ballY, {
-      toValue: 500,
-      duration: 1000,
+    // ANIMACAO SIMPLES SECA
+    // Animated.timing(ballY, {
+    //   toValue: 500,
+    //   duration: 1000,
+    //   useNativeDriver: false,
+    // }).start();
+
+    // ANIMACAO COM ELASTICO BOUNCING
+    // Animated.spring(ballY, {
+    //   toValue: 500,
+    //   bounciness: 20,
+    //   useNativeDriver: false,
+    // }).start();
+
+    // ANIMACAO ORGANICA COM VELOCIDADE DEFINIDA ATE PARAR
+    Animated.decay(ballY, {
+      velocity: 1,
       useNativeDriver: false,
     }).start();
-  }, []);
+  }, [ballY]);
 
   return (
-   <View style={styles.container}>
-     <Animated.View style={[
-        styles.ball,
-        {
-          top: ballY,
-        }
-       ]}
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          styles.ball,
+          {
+            top: ballY,
+          },
+        ]}
       />
-   </View>
+    </View>
   );
 };
 
